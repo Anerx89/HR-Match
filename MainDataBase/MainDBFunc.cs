1b8cd3795b211f6b391e856528e46f2270cc5736
Alexander Helm <allex91@live.se>
2022-11-07 15:07:22 +0100

    Added sqlString

--- MainDataBase/MainDBFunc.cs
+++ MainDataBase/MainDBFunc.cs
@@ -4,23 +4,24 @@ using MySqlConnector;
 namespace MainDataBase;
 public class MainDBFunc
 {
+    protected string sqlString = "Server=localhost;Database=hr_match;Uid=Alexander;Pwd=;";
 
     public void AddJobSeekerToDB(IsData newJobSeeker)
     {
-        var connection = new MySqlConnection("Server=localhost;Database=hr_match;Uid=Alexander;Pwd=;");
+        var connection = new MySqlConnection(sqlString);
         string sqlQuery = "INSERT INTO job_seeker (seeker_name, seeker_age, seeker_email, password) VALUES (@seeker_name, @seeker_age, @seeker_email, @password);";
         var rowsAffected = connection.Execute(sqlQuery, newJobSeeker);
 
     }
     public void AddCompanyToDB(IsData newComp)
     {
-        var connection = new MySqlConnection("Server=localhost;Database=hr_match;Uid=Alexander;Pwd=;");
+        var connection = new MySqlConnection(sqlString);
         string sqlQuery = "INSERT INTO company (c_name, c_location, c_work_area, c_email, password) VALUES (@c_name, @c_location, @c_work_area, @c_email, @password);";
         var rowsAffected = connection.Execute(sqlQuery, newComp);
     }
     public void AddJobToDB(IsData newJob)
     {
-        var connection = new MySqlConnection("Server=localhost;Database=hr_match;Uid=Alexander;Pwd=;");
+        var connection = new MySqlConnection(sqlString);
         string sqlQuery = "INSERT INTO job (job_title, job_description, job_location) VALUES (@job_title, @job_description, @job_location);";
         var rowsAffected = connection.Execute(sqlQuery, newJob);
     }
@@ -28,7 +29,7 @@ public class MainDBFunc
     public List<JobSeeker> SearchSeekerInDB()
     {
         List<JobSeeker> loggedInUser = new();
-        var connection = new MySqlConnection("Server=localhost;Database=hr_match;Uid=Alexander;Pwd=;");
+        var connection = new MySqlConnection(sqlString);
         var data = connection.Query<JobSeeker>("SELECT seeker_id, seeker_email, password FROM job_seeker;").ToList();
         foreach (JobSeeker j in data)
         {
@@ -39,7 +40,7 @@ public class MainDBFunc
     public List<Company> SearchCompanyInDB()
     {
         List<Company> loggedInCompany = new();
-        var connection = new MySqlConnection("Server=localhost;Database=hr_match;Uid=Alexander;Pwd=;");
+        var connection = new MySqlConnection(sqlString);
         var data = connection.Query<Company>("SELECT seeker_id, seeker_email, password FROM job_seeker;").ToList();
         foreach (Company c in data)
         {
